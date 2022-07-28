86f344a2e3c4b4e1ff9c62709484a3a433bab23c
Emre Ozturk <emre.ozturk@ebebek.com>
2022-07-28 07:58:48 +0300

    Switch-Case eklendi

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -27,7 +27,7 @@ public class hello {
         if (ortalama >= 50 && finalnot >= 60) {
 
 
-            System.out.printf("Ortalamanız : " + ortalama + " Final notunuz : " + finalnot + " Tebrikler geçtiniz");
+            System.out.printf("Ortalamanız : " + ortalama + " Final notunuz : " + finalnot + " Tebrikler gectiniz");
 
         } else if (ortalama >= 50 && finalnot <= 59) {
 
@@ -38,6 +38,53 @@ public class hello {
         } else {
             System.out.printf(" Ortalamanız : " + ortalama + " Final notunuz : " + finalnot + " kaldiniz ortalamaniz ve Final notunuz sinirin altinda !");
 
+        }
+
+
+        System.out.printf("\n");
+
+
+        System.out.printf("Kacinci ayi ogrenmek istersiniz ?");
+
+
+        int aylar = input.nextInt();
+        switch (aylar) {
+            case 1:
+                System.out.println("Ocak");
+                break;
+            case 2:
+                System.out.println("Şubat");
+                break;
+            case 3:
+                System.out.println("Mart");
+                break;
+            case 4:
+                System.out.println("Nisan");
+                break;
+            case 5:
+                System.out.println("Mayıs");
+                break;
+            case 6:
+                System.out.println("Haziran");
+                break;
+            case 7:
+                System.out.println("Temmuz");
+                break;
+            case 8:
+                System.out.println("Agustos");
+                break;
+            case 9:
+                System.out.println("Eylul");
+                break;
+            case 10:
+                System.out.println("Ekim");
+                break;
+            case 11:
+                System.out.println("Kasim");
+                break;
+            case 12:
+                System.out.println("Aralik");
+                break;
         }
 
 
